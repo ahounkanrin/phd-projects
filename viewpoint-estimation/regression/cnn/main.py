8c93d04458d0e3a7d788ea0d22e3203aa7a43840
ahounkanrin <anicet@aims.ac.za>
2020-08-25 22:49:07 +0200

    commit

--- viewpoint-estimation/regression/cnn/main.py
+++ viewpoint-estimation/regression/cnn/main.py
@@ -26,8 +26,8 @@ def read_dataset(hf5):
 def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("--epochs", default=50, type=int, help="Number of epochs")
-    parser.add_argument("--batch_size", default=16, type=int, help="Batch size")
-    parser.add_argument("--learning_rate", default=0.0001, type=float, help="Initial learning rate")
+    parser.add_argument("--batch_size", default=64, type=int, help="Batch size")
+    parser.add_argument("--learning_rate", default=0.00001, type=float, help="Initial learning rate")
     parser.add_argument("--is_training", default=True, type=lambda x: bool(int(x)), help="Training or testing mode")
     return parser.parse_args()
 
@@ -38,33 +38,34 @@ def preprocess(x, y):
     y = tf.constant(np.pi/180.0, dtype=tf.float32) * y
     return x, y
 
+def myMSE(images, labels):
+    gt_sin = tf.math.sin(labels)
+    gt_cos = tf.math.cos(labels)
+    predictions = model(images)
+    pred_sin = tf.math.sin(predictions)
+    pred_cos = tf.math.cos(predictions)
+    loss_sin = tf.math.squared_difference(gt_sin, pred_sin)
+    loss_sin = tf.math.reduce_mean(loss_sin)
+    loss_cos = tf.math.squared_difference(gt_cos, pred_cos)
+    loss_cos = tf.math.reduce_mean(loss_cos)
+    loss = loss_cos + loss_sin
+    
+    return loss
+
+
 args = get_arguments()
 
 # Load dataset
 DIR = "/scratch/hnkmah001/Datasets/ctfullbody/larger_fov_with_background/"
 x_train, y_train, x_val, y_val, x_test, y_test = read_dataset(DIR+'chest_fov_400x400_sparse_labels.h5')
 
-"""
-x_train = tf.constant(x_train/255.0, dtype=tf.float32)
-x_val = tf.constant(x_val/255.0, dtype=tf.float32)
-x_test = tf.constant(x_test/255.0, dtype=tf.float32)
-
-y_train = tf.constant((np.pi/180.0) * y_train, dtype=tf.float32) # Convert angles from degrees to radians
-y_val = tf.constant((np.pi/180.0) * y_val, dtype=tf.float32)
-y_test = tf.constant((np.pi/180.0)* y_test, dtype=tf.float32)"""
-
 train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(len(x_train)).batch(args.batch_size) 
-#train_data.map(preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 val_data = tf.data.Dataset.from_tensor_slices((x_val, y_val)).batch(args.batch_size)
-#val_data.map(preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE )
 test_data = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(1)
-#test_data.map(preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
 print("[INFO] Datasets created...")
 
 # Define the model
-#strategy = tf.distribute.MirroredStrategy()
-#with strategy.scope():
 
 baseModel = tf.keras.applications.InceptionV3(input_shape=(400, 400, 3), include_top=False, weights="imagenet")
 #baseModel.trainable = False
@@ -74,43 +75,33 @@ x = tf.keras.layers.Dense(1024, activation=tf.keras.activations.relu)(x)
 x = tf.keras.layers.Dense(1, activation=tf.keras.activations.sigmoid)(x)
 outputs = tf.multiply(x, tf.constant(2*np.pi))
 model = tf.keras.Model(inputs=baseModel.input, outputs=outputs)
-#model.summary()
+model.summary()
 
 # Define cost function, optimizer and metrics
-loss_object_sin = tf.keras.losses.MeanSquaredError()
-loss_object_mse = tf.keras.losses.MeanSquaredError()
+#loss_object_sin = tf.keras.losses.MeanSquaredError()
 lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(args.learning_rate, decay_steps=1000, 
                                                             decay_rate=0.96, staircase=True)
 optimizer = tf.keras.optimizers.SGD(learning_rate=lr_schedule)
-train_loss_sin = tf.keras.metrics.MeanSquaredError(name="train_loss_sin")
-train_loss_mse = tf.keras.metrics.MeanSquaredError(name="train_loss_mse")
-test_loss_sin = tf.keras.metrics.MeanSquaredError(name="test_loss_sin")
-test_loss_mse = tf.keras.metrics.MeanSquaredError(name="test_loss_mse")
+#train_loss_sin = tf.keras.metrics.MeanSquaredError(name="train_loss_sin")
+#test_loss_sin = tf.keras.metrics.MeanSquaredError(name="test_loss_sin")
 
 @tf.function
 def train_step(images, labels):
-    with tf.GradientTape() as tape_sin:
-        predictions_sin = model(images)
-        loss_sin = loss_object_sin(tf.math.sin(labels), tf.math.sin(predictions_sin))
+    with tf.GradientTape() as tape:
+        #predictions_sin = model(images)
+        loss = myMSE(images, labels)
            
-    with tf.GradientTape() as tape_mse:
-        predictions_mse = model(images)
-        loss_mse = loss_object_mse(labels, predictions_mse)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-    gradients_sin = tape_sin.gradient(loss_sin, model.trainable_variables)
-    gradients_mse = tape_mse.gradient(loss_mse, model.trainable_variables)
-    optimizer.apply_gradients(zip(gradients_sin, model.trainable_variables))
-    optimizer.apply_gradients(zip(gradients_mse, model.trainable_variables))
+    #train_loss_sin.update_state(tf.math.sin(labels), tf.math.sin(predictions_sin))
+    return loss
 
-    train_loss_sin.update_state(tf.math.sin(labels), tf.math.sin(predictions_sin))
-    train_loss_mse.update_state(labels, predictions_mse)
-    
 
 @tf.function
 def test_step(images, labels):
-    predictions = model(images)
-    test_loss_sin.update_state(tf.math.sin(labels), tf.math.sin(predictions))
-    test_loss_mse.update_state(labels, predictions)
+    return myMSE(images, labels)
+    
     
 # Define checkpoint manager to save model weights
 checkpoint = tf.train.Checkpoint(model=model, optimizer=optimizer)
@@ -134,34 +125,37 @@ if args.is_training:
     # Training loop
     step = 0
     for epoch in tqdm(range(args.epochs)):
-        for images, labels in tqdm(train_data.map(preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE), desc="Training"):
-            train_step(images, labels)
+        for images, labels in tqdm(train_data.map(preprocess, 
+                                   num_parallel_calls=tf.data.experimental.AUTOTUNE), desc="Training"):
+            loss = train_step(images, labels)
+            print("Training loss:", loss.numpy())
             if step == 0:
                 with train_summary_writer.as_default():
                     tf.summary.trace_export(name="InceptionV3", step=0)
             step += 1
             with train_summary_writer.as_default():
-                tf.summary.scalar("loss_sin", train_loss_sin.result(), step=step)
-                tf.summary.scalar("loss_mse", train_loss_mse.result(), step=step)
+                tf.summary.scalar("loss", loss, step=step)
                 tf.summary.image("image", images, step=step, max_outputs=8)
 
-        for test_images, test_labels in tqdm(val_data.map(preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE), desc="Validation"):
-            test_step(test_images, test_labels)
+        test_it = 0
+        loss_val = 0.
+        for test_images, test_labels in tqdm(val_data.map(preprocess, 
+                                             num_parallel_calls=tf.data.experimental.AUTOTUNE), desc="Validation"):
+            loss_val += test_step(test_images, test_labels)
+            test_it += 1
+        loss_val = loss_val/tf.constant(test_it, dtype=tf.float32)
+            
         with val_summary_writer.as_default():
-            tf.summary.scalar("val_loss_sin", test_loss_sin.result(), step=epoch)
-            tf.summary.scalar("val_loss_mse", test_loss_mse.result(), step=epoch)
+            tf.summary.scalar("val_loss", loss_val, step=epoch)
             tf.summary.image("val_images", test_images, step=epoch, max_outputs=8)
 
         ckpt_path = manager.save()
-        template = "\n\n\nEpoch {}, Loss-sin: {:.4f}, Loss-mse: {:.4f}, Val Loss-sin: {:.4f}, Val Loss-mse: {:.4f}, ckpt {}\n\n"
-        print(template.format(epoch+1, train_loss_sin.result(), train_loss_mse.result(), 
-              test_loss_sin.result(), test_loss_mse.result(), ckpt_path))
+        template = "\n\n\nEpoch {}, Loss: {:.4f}, Val Loss: {:.4f},  ckpt {}\n\n"
+        print(template.format(epoch+1, loss, loss_val, ckpt_path))
         
         # Reset metrics for the next epoch
         #train_loss.reset_states()
-        test_loss_sin.reset_states()
-        test_loss_mse.reset_states()
-        
+        #test_loss_sin.reset_states()        
 else:
 
     checkpoint.restore(manager.checkpoints[-1])
@@ -174,12 +168,15 @@ else:
     test_loss_mse.reset_states()"""
 
     pred = []
-    for test_images, test_labels in tqdm(test_data.map(preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE), desc="Validation"):
-            test_step(test_images, test_labels)
+    test_loss = 0.
+    count = 0
+    for test_images, test_labels in tqdm(test_data.map(preprocess, 
+                                         num_parallel_calls=tf.data.experimental.AUTOTUNE), desc="Validation"):
+            test_loss += test_step(test_images, test_labels)
             pred.append((180./np.pi)*model(test_images)) # Convert angles from radians to degrees
-    print("Test Loss-sin: {:.4f}, Test Loss-mse: {:.4f}".format(test_loss_sin.result(), test_loss_mse.result()))
+    test_loss = test_loss/tf.constant(count, dtype=tf.float32)
+    print("Test Loss: {:.4f}".format(test_loss))
 
-    
     gt = (180./np.pi)* np.array(y_test)
     pred = np.array(pred)
     pred = np.squeeze(pred)
