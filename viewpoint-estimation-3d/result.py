35ac135ed06f8cee3ee4773881d6d33ade9f0235
ahounkanrin <anicet@aims.ac.za>
2020-10-15 09:40:25 +0200

    commit

--- viewpoint-estimation-3d/result.py
+++ viewpoint-estimation-3d/result.py
@@ -5,25 +5,25 @@ print("INFO: Plotting...")
 thresholds = [theta for theta in range(0, 60, 5)]
 
 
-medErr_soft_classification_200x200 = 7.0
-acc_soft_classification_200x200 = [0.0278, 0.4056, 0.6111, 0.6806, 0.6944, 0.7083, 0.7278, 0.7528, 0.7639, 0.7778, 0.8028, 0.8111]
+medErr_soft_classification = 9.0
+acc_soft_classification = [0.0222, 0.3194, 0.5861, 0.7083, 0.7500, 0.7611, 0.7639, 0.7722, 0.7750, 0.7917, 0.8000, 0.8083]
 
-medErr_soft_classification_3d = 34.5
-acc_soft_classification_3d = [0.0139, 0.1528, 0.2528, 0.3583, 0.4278, 0.4667, 0.4806, 0.5083, 0.5250, 0.5444,  0.5694, 0.6000]
+medErr_soft_classification_3d = 11.0
+acc_soft_classification_3d = [0.0139, 0.2889, 0.4694, 0.5889, 0.6222, 0.6333, 0.6417, 0.6417, 0.6444, 0.6611, 0.6778, 0.6861]
 
 plt.figure(figsize=[8, 5])
 #plt.title("Accuracy")
 plt.ylabel("Accuracy")
 plt.xlabel("Maximum error (degrees)")
 
-plt.plot(thresholds, acc_soft_classification_200x200, label="2D Input")
+plt.plot(thresholds, acc_soft_classification, label="2D Input")
 plt.plot(thresholds, acc_soft_classification_3d, label="3D Input")
 plt.legend(loc="upper left")
 plt.grid(True)
 plt.savefig("azimuth_accuracy.png")
 
 
-medErrs = [medErr_soft_classification_200x200, medErr_soft_classification_3d]
+medErrs = [medErr_soft_classification, medErr_soft_classification_3d]
 
 plt.figure(figsize=[4, 4])
 #plt.title("Median Error")
