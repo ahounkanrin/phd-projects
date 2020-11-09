fe09c94219c740bfe5a811e3a96a4f539196f5c9
ahounkanrin <anicet@aims.ac.za>
2020-11-09 08:32:04 +0200

    commit

--- viewpoint-estimation-3d/geom-loss/result.py
+++ viewpoint-estimation-3d/geom-loss/result.py
@@ -12,8 +12,10 @@ acc_geom_loss = [0.0000, 0.3806, 0.5556, 0.6500, 0.7167, 0.7361, 0.7528, 0.7639,
 #acc_geom_loss_3d = [0.0194, 0.4417, 0.6750, 0.7583, 0.7833, 0.7917, 0.7944, 0.7944, 0.7944, 0.7944, 0.7944, 0.7972]
 
 medErr_geom_loss_3d_inplane_rotation = 8.0
-acc_geom_loss_3d_inplane_rotation = [0.0333, 0.3417, 0.5833, 0.7306, 0.8194, 0.8472, 0.8667, 0.8667, 0.8667, 0.8667, 0.8667, 0.8667]
+acc_geom_loss_3d_inplane_rotation = [0.0306, 0.3500, 0.6028, 0.7444, 0.8250, 0.8472, 0.8667, 0.8667, 0.8667, 0.8667, 0.8667, 0.8667]
 
+medErr_geom_loss_3d_outofplane_rotation = 9.0
+acc_geom_loss_3d_outofplane_rotation = [0.0250, 0.3111, 0.5250, 0.6111, 0.6611, 0.6833, 0.7306, 0.7500, 0.7583, 0.7694, 0.7889, 0.7972]
 plt.figure(figsize=[8, 5])
 #plt.title("Accuracy")
 plt.ylabel("Accuracy")
@@ -21,21 +23,23 @@ plt.xlabel("Threshold (degrees)")
 
 plt.plot(thresholds, acc_geom_loss, label="2D")
 #plt.plot(thresholds, acc_geom_loss_3d, label="3D")
-plt.plot(thresholds, acc_geom_loss_3d_inplane_rotation, label="3D + Ry rotation")
+plt.plot(thresholds, acc_geom_loss_3d_inplane_rotation, label="3D + in-plane rotation")
+plt.plot(thresholds, acc_geom_loss_3d_outofplane_rotation, label="3D + out-of-plane rotation")
 plt.legend(loc="lower right")
 plt.grid(True)
 plt.savefig("accuracy.png")
 
 
-medErrs = [medErr_geom_loss, medErr_geom_loss_3d_inplane_rotation]
+medErrs = [medErr_geom_loss, medErr_geom_loss_3d_inplane_rotation, medErr_geom_loss_3d_outofplane_rotation]
 
 plt.figure(figsize=[4, 4])
 #plt.title("Median Error")
 plt.ylabel("Median Error (degrees)")
 plt.bar(0, medErrs[0],  label="2D")
 #plt.bar(1, medErrs[1],  label="3D")
-plt.bar(1, medErrs[1],  label="3D + In-plane rotation")
-plt.xticks([0, 1], [])
+plt.bar(1, medErrs[1],  label="3D + in-plane rotation")
+plt.bar(2, medErrs[2], label="3D + out-of-plane rotation")
+plt.xticks([0, 1, 2], [])
 plt.legend(loc="upper right")
 plt.savefig("mederr.png")
 
