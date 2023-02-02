9fd37729cbfcd2252dfb562d434d74faf3c07e02
ahounkanrin <anicet@aims.ac.za>
2023-02-02 14:34:55 +0200

    project backup

---
+++ DRR/FVR-2D/fvr_zero_padding.py
@@ -0,0 +1,172 @@
+import nibabel as nib
+import numpy as np
+import time
+import cv2 as cv
+from scipy.interpolate import interpn
+from scipy.fft import fftn, fftshift, ifft2, ifft
+from matplotlib import pyplot as plt
+from scipy import ndimage
+from scipy.interpolate import Rbf
+
+np.random.seed(0)
+N = 400
+def Rx(theta):
+    x = theta * np.pi / 180.
+    r11, r12 = np.cos(x), -np.sin(x)
+    r21, r22 = np.sin(x), np.cos(x)
+    return np.array([[r11, r12], [r21, r22]])
+
+def rotate_line(line, rotationMatrix):
+	return np.matmul(rotationMatrix, line)
+
+def normalize(img):
+    img = (img - np.min(img))/(np.max(img) - np.min(img))
+    img = 255 * img 
+    return np.uint8(img)
+
+if __name__ == "__main__":
+
+	img = cv.imread("0_fvr.png", 0)
+	#img = ndimage.rotate(img, 45, reshape=False)
+	# img = np.pad(img, N//2, "constant", constant_values=0)
+	img = np.pad(img, N, "constant", constant_values=0)
+	print(img.shape)
+	imgShifted = fftshift(img)
+	imgFFT = fftn(imgShifted)
+	imgFFTShifted = fftshift(imgFFT)
+	imgFFTShiftedReal = imgFFTShifted.real
+	imgFFTShiftedImag = imgFFTShifted.imag
+	print("2D FFT computed.")
+
+
+	# Rotation and Interpolation of the projection slice from the 3D FFT volume
+
+	# x = np.linspace(-N+0.5, N-0.5, 2*N)
+	# y = np.linspace(-N+0.5, N-0.5, 2*N)
+	# x = np.linspace(-N//2+0.5, N//2-0.5, N)
+	# y = np.linspace(-N//2+0.5, N//2-0.5, N)
+	
+	x = np.linspace(-3*N//2+0.5, 3*N//2-0.5, 3*N)
+	y = np.linspace(-3*N//2+0.5, 3*N//2-0.5, 3*N)
+	xx, yy = np.meshgrid(x, y)
+	xx = xx.flatten()
+	yy = yy.flatten()
+	data = imgFFTShifted.flatten()
+	# rbfi = Rbf(xx, yy, data, function="cubic")
+	projectionLine = np.array([[xi, 0.] for xi in x])
+	# projectionLine = np.reshape(projectionLine, (N, 2, 1))
+	# projectionLine = np.reshape(projectionLine, (2*N, 2, 1))
+	projectionLine = np.reshape(projectionLine, (3*N, 2, 1))
+
+	def render_view(theta, interpMethod):
+		tic_rendering = time.time()
+		rotationMatrix = Rx(theta)
+		projectionSlice = np.squeeze(rotate_line(projectionLine, rotationMatrix))
+		projectionSliceFFTReal = interpn(points=(x, y), values=imgFFTShiftedReal, xi=projectionSlice, method=interpMethod,
+				                     bounds_error=False)   #, fill_value=0
+		projectionSliceFFTImag = interpn(points=(x, y), values=imgFFTShiftedImag, xi=projectionSlice, method=interpMethod,
+				                     bounds_error=False) 
+		projectionSliceFFT = projectionSliceFFTReal + 1j * projectionSliceFFTImag			                     
+		projection = fftshift(ifft(projectionSliceFFT))  
+		projection = np.abs(projection)
+		#projection = normalize(projection)
+		toc_rendering = time.time()
+		print("theta = {}\t {:.2f} seconds".format(theta, toc_rendering-tic_rendering))
+		return projection
+
+	# def render_view_rbf(theta):
+	# 	tic_rendering = time.time()
+	# 	rotationMatrix = Rx(theta)
+	# 	projectionSlice = np.squeeze(rotate_line(projectionLine, rotationMatrix))
+	# 	projectionSlice = np.reshape(projectionSlice, (2, N))
+	# 	projectionSliceFFT = rbfi(projectionSlice)
+	# 	projection = fftshift(ifft(projectionSliceFFT))  
+	# 	projection = np.abs(projection)
+	# 	#projection = normalize(projection)
+	# 	toc_rendering = time.time()
+	# 	print("theta = {}\t {:.2f} seconds".format(theta, toc_rendering-tic_rendering))
+	# 	return projection
+		
+	img_x = np.sum(img, axis=1)
+	img_x_mean = np.mean(img_x)
+	img_y = np.sum(img, axis=0)
+	img_y_mean = np.mean(img_y)
+	projection_x_spline = render_view(0, "splinef2d") 
+	projection_x_spline_mean = np.mean(projection_x_spline)
+	projection_y_spline = render_view(90, "splinef2d")
+	projection_y_spline_mean = np.mean(projection_y_spline)
+	# projection_x_linear = render_view(0, "linear") 
+	# projection_y_linear = render_view(90, "linear") 
+	# projection_x_nearest = render_view(0, "nearest") 
+	# projection_y_nearest = render_view(90, "nearest") 
+	# projection_x_cubic = render_view_rbf(0)
+	# projection_y_cubic = render_view_rbf(90)
+	#img_x = normalize(img_x)
+	#img_y = normalize(img_y)
+	#projection_x = normalize(projection_x)
+	#projection_y = normalize(projection_y)
+
+
+	# plt.figure(figsize=(8, 6))
+	# plt.title(r"Projection onto the x-axis (2X padding)")
+	# plt.plot(img_x, label="Direct projection")
+	# plt.plot(projection_x_nearest, label=r"FVR -- nearest neighbor")
+	# plt.plot(projection_x_linear, label=r"FVR -- linear")
+	# plt.plot(projection_x_spline, label=r"FVR -- spline")
+	# plt.plot(projection_x_cubic, label="FVR -- cubic")
+	# plt.legend(loc="upper left")
+	# plt.grid(True)
+	# plt.savefig("xprojection_padding_2X_spline.png")
+
+	plt.figure()
+	# plt.title(r"Projection onto the x-axis (no padding)")
+	plt.plot(img_x, label="AP")
+	plt.plot(projection_x_spline, label=r"FVR")
+	plt.legend(loc="upper left")
+	plt.grid(True)
+	plt.savefig("ap_fvr_x3.png")
+
+
+	plt.figure()
+	# plt.title(r"Projection onto the x-axis (no padding)")
+	plt.plot(img_y, label="AP")
+	plt.plot(projection_y_spline, label=r"FVR")
+	plt.legend(loc="upper left")
+	plt.grid(True)
+	plt.savefig("ap_fvr_y3.png")
+
+
+	# plt.figure(figsize=(8, 6))
+	# plt.title(r"Projection onto the y-axis (2X padding)")
+	# plt.plot(img_y, label="Direct projection")
+	# plt.plot(projection_y_nearest, label=r"FVR -- nearest neighbor")
+	# plt.plot(projection_y_linear, label=r"FVR -- linear")
+	# plt.plot(projection_y_spline, label=r"FVR -- spline")
+	# plt.legend(loc="upper left")
+	# plt.grid(True)
+	# plt.savefig("yprojection_padding_2X_spline.png")
+	
+
+	# plt.figure(figsize=(20, 8))
+	# plt.subplot(2, 2, 1)
+	# plt.grid(True)
+	# plt.plot(img_x, label="Direct projection")
+	# plt.plot(projection_x_spline, label="FVR -- spline")
+	# #plt.grid(True)
+
+	# plt.subplot(2, 2, 2)
+	# plt.grid(True)
+	# plt.plot(img_x, label="Direct projection")
+	# plt.plot(img_x_mean - projection_x_spline_mean + projection_x_spline, label="FVR -- spline")
+	
+	# plt.subplot(2, 2, 3)
+	# plt.grid(True)
+	# plt.plot(img_y, label="Direct projection")
+	# plt.plot(projection_y_spline, label="FVR -- spline")
+	
+	# plt.subplot(2, 2, 4)
+	# plt.grid(True)
+	# plt.plot(img_y, label="Direct projection")
+	# plt.plot(img_y_mean - projection_y_spline_mean + projection_y_spline, label="FVR -- spline")
+	# plt.savefig("xyprojection_shifted_2X.png")
+	
--- results_3d.py
+++ results_3d.py
@@ -8,7 +8,8 @@ az_el = [0.0000, 0.7802, 0.9116, 0.9498, 0.9656, 0.9776, 0.9848, 0.9902, 0.9959,
 az_el_ip =  [0.0000, 0.6921, 0.8775, 0.9182, 0.9396, 0.9565, 0.9707, 0.9832, 0.9925, 1.0000] 
 
 #quaternion = [0.0000, 0.7205, 0.8872, 0.9330, 0.9586, 0.9724, 0.9806, 0.9878, 0.9942, 1.0000]
-quaternion = [0.0000, 0.7720, 0.8620, 0.9020, 0.9260, 0.9500, 0.9690, 0.9800, 0.9910, 1.0000]
+# quaternion = [0.0000, 0.7720, 0.8620, 0.9020, 0.9260, 0.9500, 0.9690, 0.9800, 0.9910, 1.0000]
+quaternion = [0.0000, 0.6561, 0.8214, 0.8787, 0.9151, 0.9395, 0.9572, 0.9713, 0.9856, 1.0000]
 classification_500 = [0.0, 0.29725, 0.54625, 0.6125, 0.683, 0.73675, 0.7845, 0.855, 0.93075, 1.0]
 #classification_1000 = [0.7321, 0.9646, 0.9719, 0.9735, 0.9754, 0.9794, 0.9841, 0.9894, 0.9955, 1.0000]
 classification_1000 = [0.0000, 0.4773, 0.6944, 0.7416, 0.7800, 0.8153, 0.8542, 0.9106, 0.9617, 1.0000]
@@ -17,6 +18,11 @@ classification_2000 = [0.0000, 0.6637, 0.7906, 0.8233, 0.8548, 0.8827, 0.9091, 0
 #classification_5000 = [0.6325, 0.9071, 0.9240, 0.9354, 0.9489, 0.9625, 0.9745, 0.9831, 0.9920, 1.0000]
 classification_5000 = [0.0000, 0.8433, 0.8890, 0.9090, 0.9276, 0.9444, 0.9597, 0.9717, 0.9849, 1.0000]
 classification_20000 = [0.0000, 0.8434, 0.8760, 0.8948, 0.9129, 0.9313, 0.9485, 0.9656, 0.9825, 1.0000]
+
+reg_1k = [0.0000, 0.4445, 0.6910, 0.7931, 0.8521, 0.8955, 0.9291, 0.9625, 0.9829, 1.0000]
+reg_2k = [0.0000, 0.4234, 0.6911, 0.8027, 0.8682, 0.9141, 0.9477, 0.9684, 0.9856, 1.0000]
+reg_5k = [0.0000, 0.6561, 0.8214, 0.8787, 0.9151, 0.9395, 0.9572, 0.9713, 0.9856, 1.0000]
+reg_20k = [0.0000, 0.7720, 0.8620, 0.9020, 0.9260, 0.9500, 0.9690, 0.9800, 0.9910, 1.0000]
 plt.figure(figsize=[8, 5])
 #plt.title("Accuracy of the CNN")
 #plt.figure()
@@ -27,12 +33,77 @@ plt.yticks(ticks=[i/10 for i in range(11)])
 
 #plt.plot(thresholds, az_el, label="Azimuth-Elevation-to-quaternions")
 #plt.plot(thresholds, az_el_ip, label="Regression: Euler-to-quaternions")
-plt.plot(thresholds, quaternion, label="Regression")
-plt.plot(thresholds, classification_500, label="Classification-500-classes")
-plt.plot(thresholds, classification_1000, label="Classification-1000-classes")
-plt.plot(thresholds, classification_2000, label="Classification-2000-classes")
-plt.plot(thresholds, classification_5000, label="Classification-5000-classes")
-plt.plot(thresholds, classification_20000, label="Classification-20000-classes")
+# plt.plot(thresholds, quaternion, label="Regression")
+# plt.plot(thresholds, classification_500, label="Classification-500-classes")
+plt.plot(thresholds, classification_1000, label="1000 quaternions")
+plt.plot(thresholds, classification_2000, label="2000 quaternions")
+plt.plot(thresholds, classification_5000, label="5000 quaternions")
+plt.plot(thresholds, classification_20000, label="20000 quaternions")
 plt.legend(loc="lower right")
 plt.grid(True)
-plt.savefig("accuracy_quaternion_reg_classification.png")
+plt.savefig("accuracy_quaternion_classification.png")
+
+plt.figure(figsize=[8, 5])
+#plt.title("Accuracy of the CNN")
+#plt.figure()
+plt.ylabel("Accuracy")
+plt.xlabel("Threshold (degrees)")
+plt.xticks(ticks=[i for i in range(0, 95, 10)])
+plt.yticks(ticks=[i/10 for i in range(11)])
+
+plt.plot(thresholds, reg_1k, label="1000 quaternions")
+plt.plot(thresholds, reg_2k, label="2000 quaternions")
+plt.plot(thresholds, reg_5k, label="5000 quaternions")
+plt.plot(thresholds, reg_20k, label="20000 quaternions")
+plt.legend(loc="lower right")
+plt.grid(True)
+plt.savefig("accuracy_quaternion_regression.png")
+
+
+acc_theta_reg = [0.79, 0.80, 0.88, 0.90]
+acc_theta_class = [0.74, 0.82, 0.91, 0.89]
+
+med_error_reg = [11.44, 11.95, 6.9, 5.05]
+med_error_class = [10.51, 7.31, 4.84, 3.65]
+
+labels = ["1K", "2K", "5K", "20K"]
+x = np.arange(len(labels))  # the label locations
+width = 0.35 # the width of the bars
+
+fig, ax = plt.subplots(figsize=[8, 5])
+rects1 = ax.bar(x - width/2, acc_theta_reg, width, label='Regression')
+rects2 = ax.bar(x + width/2, acc_theta_class, width, label='Classification')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Accuracy')
+ax.set_xlabel("Number of sampled quaternions.")
+# ax.set_title('Scores by group and gender')
+ax.set_xticks(x, labels)
+ax.set_yticks(np.arange(0, 1.1, 0.1))
+ax.legend()
+
+ax.bar_label(rects1)
+ax.bar_label(rects2)
+
+fig.tight_layout()
+
+plt.savefig("barplot_accuracy.png")
+
+fig, ax = plt.subplots(figsize=[8, 5])
+rects1 = ax.bar(x - width/2, med_error_reg, width, label='Regression')
+rects2 = ax.bar(x + width/2, med_error_class, width, label='Classification')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Error (degrees)')
+ax.set_xlabel("Number of sampled quaternions")
+# ax.set_title('Scores by group and gender')
+ax.set_xticks(x, labels)
+# ax.set_yticks(np.arange(0, 1.1, 0.1))
+ax.legend()
+
+ax.bar_label(rects1)
+ax.bar_label(rects2)
+
+fig.tight_layout()
+
+plt.savefig("barplot_error.png")
